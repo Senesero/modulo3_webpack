4a790a350a25349fc3b1dcda07e8d0d6facd78ec
Senesero <senesero@gmail.com>
2018-11-17 12:15:46 +0100

    Realizado ejercicio laboratorio Webpack con SASS

---
+++ modulo3_webpack/laboratorio/webpack_sass/webpack.config.js
@@ -0,0 +1,38 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var webpack = require('webpack');
+var path = require('path');
+
+var basePath = __dirname;
+
+module.exports = {
+    context: path.join(basePath, 'src'),
+    entry: {
+        appStyles: [
+            './mystyles.scss',
+        ],
+    },
+    output: {
+        filename: '[name].[chunkhash].bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                "css-loader",
+                "sass-loader",
+            ]
+        }, ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html', // este esta en ./dist
+            template: 'index.html', // este esta en ./
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
+}
